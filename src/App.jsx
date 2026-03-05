a753c949c1d135d32f4c75ff9e8d353a88d3b18b
Toba <riemehasan06@gmail.com>
2026-03-05 23:52:37 +0600

    header footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
+import './index.css'
+import Footer from './layouts/Footer'
+import Header from './layouts/Header'
 
 function App() {
   
   return (
     <>
-    
+    <Header/>
+    <Footer/>
     </>
   )
 }
