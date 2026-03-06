48d91f9ee79ff3046f7864a301d789bd50e4f373
Toba <riemehasan06@gmail.com>
2026-03-06 23:37:59 +0600

    icons

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,28 +1,48 @@
-import Image from "../Image"
-import logo from "/src/assets/logo.png"
-
+import Image from "../Image";
+import logo from "/src/assets/logo.png";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 const Header = () => {
   return (
     <div className="bg-[#F4F4F4] py-5">
-        <div className="container flex items-center justify-between">
-            <div className="">
-              <Image src={logo}/>
-            </div>
-            <div className="">
-              <ul className="flex items-center gap-x-5">
-                <li className="font-rajdhani text-base font-bold text-common">Home</li>
-                <li className="font-rajdhani text-base font-bold text-common">About</li>
-                <li className="font-rajdhani text-base font-bold text-common">Services</li>
-                <li className="font-rajdhani text-base font-bold text-common">Blog</li>
-                <li className="font-rajdhani text-base font-bold text-common">Project</li>
-                <li className="font-rajdhani text-base font-bold text-common">Contact</li>
-              </ul>
-            </div>
+      <div className="container flex items-center justify-between">
+        <div className="">
+          <Image src={logo} />
         </div>
-
+        <div className="">
+          <ul className="flex items-center gap-x-5">
+            <li className="font-rajdhani text-base font-bold text-common">
+              Home
+            </li>
+            <li className="font-rajdhani text-base font-bold text-common">
+              About
+            </li>
+            <li className="font-rajdhani text-base font-bold text-common flex items-center gap-1">
+              Services{" "}
+              <span>
+                <MdOutlineKeyboardArrowDown />
+              </span>
+            </li>
+            <li className="font-rajdhani text-base font-bold text-common flex items-center gap-1">
+              Blog{" "}
+              <span>
+                <MdOutlineKeyboardArrowDown />
+              </span>
+            </li>
+            <li className="font-rajdhani text-base font-bold text-common flex items-center gap-1">
+              Project{" "}
+              <span>
+                <MdOutlineKeyboardArrowDown />
+              </span>
+            </li>
+            <li className="font-rajdhani text-base font-bold text-common">
+              Contact
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
