c8792f22576a3f7d8d5ba5521fd656c5d1527a51
Toba <riemehasan06@gmail.com>
2026-03-06 23:34:46 +0600

    nav

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -5,13 +5,18 @@ import logo from "/src/assets/logo.png"
 const Header = () => {
   return (
     <div className="bg-[#F4F4F4] py-5">
-        <div className="container flex items-center">
+        <div className="container flex items-center justify-between">
             <div className="">
               <Image src={logo}/>
             </div>
             <div className="">
               <ul className="flex items-center gap-x-5">
                 <li className="font-rajdhani text-base font-bold text-common">Home</li>
+                <li className="font-rajdhani text-base font-bold text-common">About</li>
+                <li className="font-rajdhani text-base font-bold text-common">Services</li>
+                <li className="font-rajdhani text-base font-bold text-common">Blog</li>
+                <li className="font-rajdhani text-base font-bold text-common">Project</li>
+                <li className="font-rajdhani text-base font-bold text-common">Contact</li>
               </ul>
             </div>
         </div>
@@ -21,9 +26,3 @@ const Header = () => {
 }
 
 export default Header
-// Home
-// About
-// Services 
-// Blog 
-// Project 
-// Contact
