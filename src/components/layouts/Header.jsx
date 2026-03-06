296ce1a75c4e09cb95bb6b9ce561bd8689416515
Toba <riemehasan06@gmail.com>
2026-03-06 23:23:35 +0600

    nav work

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,10 +1,14 @@
+import Image from "../Image"
+import logo from "/src/assets/logo.png"
 
 
 const Header = () => {
   return (
     <div className="bg-[#F4F4F4] py-5">
-        <div className="flex items-center">
-            
+        <div className="container flex items-center">
+            <div className="">
+              <Image src={logo}/>
+            </div>
         </div>
 
     </div>
