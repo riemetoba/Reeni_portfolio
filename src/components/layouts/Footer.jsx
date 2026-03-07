7eceefa29fb6d0ec495be216be277f6be72efcca
Toba <riemehasan06@gmail.com>
2026-03-08 01:33:44 +0600

    footer done

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,12 +1,15 @@
+import Icons from "../Icons";
 import Image from "../Image";
 import logo from "/src/assets/logo.png";
 import { CiMail } from "react-icons/ci";
+import { HiMail } from "react-icons/hi";
+import { IoLocationSharp, IoCall } from "react-icons/io5";
 
 const Footer = () => {
   return (
     <div className="bg-[#F4F4F4] py-15">
       <div className="container flex items-start justify-between">
-        <div className="">
+        <div className="w-[50%]">
           <Image src={logo} />
           <h2 className="text-[50px] font-rajdhani w-100 leading-15 py-8">
             <span className="font-bold">Get Ready</span> To Create Great
@@ -20,17 +23,72 @@ const Footer = () => {
             <CiMail />
           </div>
         </div>
-        <div className="">
-          <h4 className="font-rajdhani font-bold text-2xl text-common pb-8">Quick Link</h4>
-          <ul>
-            <li className="font-rubik text-base text-common pt-4">About Me</li>
-            <li className="font-rubik text-base text-common pt-4">Service</li>
-            <li className="font-rubik text-base text-common pt-4">Contact Us</li>
-            <li className="font-rubik text-base text-common pt-4">Blog Post</li>
-            <li className="font-rubik text-base text-common pt-4">Pricing</li>
-          </ul>
+        <div className="w-[50%]">
+          <div className="flex items-start justify-between">
+            <div className="">
+              <h4 className="font-rajdhani font-bold text-2xl text-common pb-8">
+                Quick Link
+              </h4>
+              <ul>
+                <li className="font-rubik text-base text-common pt-4">
+                  About Me
+                </li>
+                <li className="font-rubik text-base text-common pt-4">
+                  Service
+                </li>
+                <li className="font-rubik text-base text-common pt-4">
+                  Contact Us
+                </li>
+                <li className="font-rubik text-base text-common pt-4">
+                  Blog Post
+                </li>
+                <li className="font-rubik text-base text-common pt-4">
+                  Pricing
+                </li>
+              </ul>
+            </div>
+            <div className="">
+              <h4 className="font-rajdhani font-bold text-2xl text-common pb-10">
+                Contact
+              </h4>
+              {/* icon + content part start */}
+              <div className="flex items-center">
+                <div className="flex items-center justify-center rounded-full border-2 border-[#D9DDE0] h-10 w-10">
+                  <HiMail />
+                </div>
+                <p className="text-base font-rubik text-common ml-3">
+                  nafiz125@gmail.com
+                </p>
+              </div>
+              <div className="flex items-center mt-3">
+                <div className="flex items-center justify-center rounded-full border-2 border-[#D9DDE0] h-10 w-10">
+                  <IoLocationSharp />
+                </div>
+                <p className="text-base font-rubik text-common ml-3">
+                  3891 Ranchview Dr. Richardson
+                </p>
+              </div>
+              <div className="flex items-center mt-3 mb-8">
+                <div className="flex items-center justify-center rounded-full border-2 border-[#D9DDE0] h-10 w-10">
+                  <IoCall />
+                </div>
+                <p className="text-base font-rubik text-common ml-3">
+                  01245789321
+                </p>
+              </div>
+              <Icons />
+            </div>
+          </div>
         </div>
       </div>
+       <div className="container lg:flex md:flex justify-between items-center py-7 border-t border-[#D9DDE0] lg:mt-31.75 mt-15">
+          <p className="text-sm text-[#5D6570] font-normal font-rubik leading-7.5 text-start">
+            © InversWeb 2026 | All Rights Reserved
+          </p>
+          <p className="text-sm text-common font-normal font-rubik leading-7.5 text-start">
+            Trams & Condition Privacy Policy Contact Us
+          </p>
+        </div>
     </div>
   );
 };
